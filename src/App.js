56eba2e2a58c2cdc7845ed26064ee5edeb6ca0f7
kawcher <kawcher578@gmail.com>
2021-06-09 22:18:10 +0600

    creat-hot-service

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from './component/Footer/Footer';
 import Main from './component/Header/Main';
 import Navbar from './component/Header/Navbar';
+import Service from './component/service/Service';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Main />
+      <Service />
       <Footer />
     </div>
   );
